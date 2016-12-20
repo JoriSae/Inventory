d26b1f9e009dc84f81d3bda54e1a5852317be97b
Gun Tool Unity <1002658@student.sae.edu.au>
2016-12-20 16:53:37 +1000

    INITIAL

---
+++ Assets/Scripts/InventoryItemScript.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventoryItemScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    enum ItemState { itemStationary, itemDrag }
+    ItemState itemState;
+
+    Vector3 offset;
+
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+    {
+        switch (itemState)
+        {
+            case ItemState.itemDrag:
+                gameObject.transform.position = Input.mousePosition + offset;
+                Debug.Log("(Make invisible yo)");
+                Cursor.visible = false;
+                break;
+            case ItemState.itemStationary:
+                Debug.Log("(Make visible yo)");
+                Cursor.visible = true;
+                break;
+        }
+
+        if(itemState == ItemState.itemDrag)
+        {
+            gameObject.transform.position = Input.mousePosition + offset;
+            Debug.Log("(Make invisible yo)");
+            Cursor.visible = false;
+        }
+        else
+        {
+            Debug.Log("(Make visible yo)");
+            Cursor.visible = true;
+        }
+
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        offset = gameObject.transform.position - Input.mousePosition;
+        Debug.Log("Set to DOWN");
+        itemState = ItemState.itemDrag;
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        Debug.Log("set to UP");
+        itemState = ItemState.itemStationary;
+    }
+}
---
+++ Assets/Scripts/UIModuleScript.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIModuleScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+	void Start()
+    {
+	
+	}
+	
+	void Update()
+    {
+	
+	}
+
+    void IPointerEnterHandler.OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "Enter");
+        gameObject.GetComponent<Image>().color = Color.blue;
+        
+    }
+
+    void IPointerExitHandler.OnPointerExit(PointerEventData eventData)
+    {
+        Debug.Log(gameObject.name + "Exit");
+        gameObject.GetComponent<Image>().color = Color.white;
+    }
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+
+    }
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        if (eventData.button == 0)
+        Debug.Log(eventData.button);
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+
+    }
+}
